256fbedee7f54d51b227f2bf9ec6fa5d7cb0d8c9
Dustin-Wang <Dustin-Wang@users.noreply.github.com>
2023-04-29 15:39:18 +0800

    Prime Factor output via memoization
    How to get the prime factors list of an arbitrary number?
    Use memoization and https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/

---
+++ Leetcode/Weekly-contest-342-2023-Apr-23/6390. Sliding Subarray Beauty.cpp
@@ -0,0 +1,570 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <list>
+#include <map>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <typeinfo>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+// BEGIN NO SAD
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+// END NO SAD
+// REMEMBER CLEAR GLOBAL STATE
+
+// struct myclass{
+// 	bool operator()(string a, string b){
+// 		if(a.size()<b.size())
+// 			return false;
+// 		else if(a.size()>b.size())
+// 			return true;
+// 		else
+// 			return(a>b);
+// 	}
+// } myobject;
+
+/*
+6390. Sliding Subarray Beauty
+
+
+Given an integer array nums containing n integers, find the beauty of each subarray of size k.
+
+The beauty of a subarray is the xth smallest integer in the subarray if it is negative, or 0 if there are fewer than x negative integers.
+
+Return an integer array containing n - k + 1 integers, which denote the beauty of the subarrays in order from the first index in the array.
+
+    A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,-1,-3,-2,3], k = 3, x = 2
+Output: [-1,-2,-2]
+Explanation: There are 3 subarrays with size k = 3. 
+The first subarray is [1, -1, -3] and the 2nd smallest negative integer is -1. 
+The second subarray is [-1, -3, -2] and the 2nd smallest negative integer is -2. 
+The third subarray is [-3, -2, 3] and the 2nd smallest negative integer is -2.
+
+Example 2:
+
+Input: nums = [-1,-2,-3,-4,-5], k = 2, x = 2
+Output: [-1,-2,-3,-4]
+Explanation: There are 4 subarrays with size k = 2.
+For [-1, -2], the 2nd smallest negative integer is -1.
+For [-2, -3], the 2nd smallest negative integer is -2.
+For [-3, -4], the 2nd smallest negative integer is -3.
+For [-4, -5], the 2nd smallest negative integer is -4. 
+
+Example 3:
+
+Input: nums = [-3,1,2,-3,0,-3], k = 2, x = 1
+Output: [-3,0,-3,-3,-3]
+Explanation: There are 5 subarrays with size k = 2.
+For [-3, 1], the 1st smallest negative integer is -3.
+For [1, 2], there is no negative integer so the beauty is 0.
+For [2, -3], the 1st smallest negative integer is -3.
+For [-3, 0], the 1st smallest negative integer is -3.
+For [0, -3], the 1st smallest negative integer is -3.
+
+ 
+
+Constraints:
+
+    n == nums.length 
+    1 <= n <= 105
+    1 <= k <= n
+    1 <= x <= k 
+    -50 <= nums[i] <= 50 
+
+
+*/
+
+template<typename T>
+void printvector(vector<T> vector){
+ //    int arraysize = array.size();
+	// for(int k=0; k<arraysize; k++)
+	// 	printf("%d  ",array[k]);
+	// printf("size: %d\n",arraysize);
+	copy(vector.begin(), vector.end(), std::ostream_iterator<T>(std::cout, " "));
+	printf("\n");
+	fflush(stdout);
+};
+
+ // * Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+ // Definition for singly-linked list.
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+template<typename Q>
+void print_queue(Q q) {
+    // NB: q is passed by value because there is no way to traverse
+    // priority_queue's content without erasing the queue.
+    while(!q.empty()){
+        auto x = q.top();
+        printf("(%d,%d) ", x.first, x.second);
+        q.pop();
+    }
+    cout<<"\n";
+};
+
+void PrintQueueInIntBinaryTree(queue<TreeNode*> queue){
+    int queuesize = queue.size();
+	while(!queue.empty())
+	{
+		auto it = queue.front();
+		if(it!=nullptr)
+			printf("%d  ",it->val);
+		queue.pop();
+	}
+	printf("queue size: %d\n",queuesize);
+	fflush(stdout);
+};
+
+TreeNode* ArrayToBinaryTree(vector<string> input){
+	int length = input.size();
+	int depth = ceil(log2(double(length)));
+	//fill in the extra nullptr leaves in the given input
+	for(int i = length; i< pow(2, depth)-1; i++)
+	{
+		input.push_back("null");
+	}
+
+	printvector(input);
+	TreeNode* root = new TreeNode(stoi(input[0]));
+	queue<TreeNode*> prevLevel;
+	prevLevel.push(root);
+	int index = 1;
+	for(int k = 1; k<depth; k++)
+	{
+		queue<TreeNode*> NextLevel;
+		PrintQueueInIntBinaryTree(prevLevel);
+		while(!prevLevel.empty())
+		{
+			TreeNode* s = prevLevel.front();
+			prevLevel.pop();
+			
+			if(s == nullptr)
+			{
+				index +=2;
+				NextLevel.push(nullptr);
+				NextLevel.push(nullptr);
+				continue;
+			};
+			
+			if(input[index] == "null")
+			{
+				s->left = nullptr;
+				NextLevel.push(s->left);
+			}
+			else
+			{
+				s->left = new TreeNode(stoi(input[index]));
+				NextLevel.push(s->left);
+			};
+			index++;
+			// PrintQueueInIntBinaryTree(NextLevel);
+			if(input[index] == "null")
+			{
+				s->right = nullptr;
+				NextLevel.push(s->right);
+			}
+			else
+			{
+				s->right = new TreeNode(stoi(input[index]));
+				NextLevel.push(s->right);
+			};
+			index++;
+			// PrintQueueInIntBinaryTree(NextLevel);
+		}
+		// PrintQueueInIntBinaryTree(NextLevel);
+		prevLevel = NextLevel;
+		printf("k: %d\n", k);
+		fflush(stdout);
+	}
+
+	return(root);
+}
+
+
+ListNode* VectorToLinkedList(vector<int> input){
+	int VectorLength = input.size();
+	if(VectorLength ==0 ){
+		return(nullptr);
+	}
+	ListNode* res= new ListNode(input[0]);
+	ListNode* tmp = res;
+	for(int i=1; i< VectorLength; i++){
+		tmp->next = new ListNode(input[i]);
+		tmp = tmp->next;
+	}
+	return(res);
+}
+
+void printLinkedList(ListNode* input){
+	ListNode* tmp = input;
+	while(tmp!=nullptr){
+		printf("%d ", tmp->val);
+
+		tmp = tmp->next;
+	}
+	fflush(stdout);
+}
+//we know that gcd(a,b)=ax+by for some x and y, here we try to keep track of the x and y
+int gcd(int a, int b, int &x, int &y){
+	if(b==0){
+		x=1;
+		y=0;
+		return(a);
+	}
+	else{
+		int c = a/b;
+		int res = gcd(b,a%b, x, y);
+		int tmp = x;
+		x=y;
+		y=tmp-c*y;
+		return(res);
+	}
+}
+
+
+bool comparePairs(const pair<int,vector<int>>& lhs, const pair<int,vector<int>>& rhs)
+{
+	return (lhs.first < rhs.first);
+}
+
+
+
+struct CompareFuncWithParams{
+    explicit CompareFuncWithParams(vector<vector<int>> grid_) 
+    : grid(grid_) {}
+
+    bool operator ()(const pair<int,int>& a, const pair<int,int>& b) const {
+        return (grid[a.first][a.second]>grid[b.first][b.second]);
+    }
+    vector<vector<int>> grid;
+};
+
+typedef priority_queue<pair<int,int>, vector<pair<int,int>>, CompareFuncWithParams> myPriorityQueue;
+
+int quick_pow10(int n)
+{
+	static int pow10[10]={
+        1, 10, 100, 1000, 10000, 
+        100000, 1000000, 10000000, 100000000, 1000000000
+    };
+    return pow10[n];
+}
+int LargePrime = pow(10,9)+7;
+
+
+
+class Solution {
+public:
+    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+        int numsLen = nums.size();
+        for(int i=0; i< numsLen; i++)
+        	nums[i]= min(nums[i],0);
+        vector<int> res;
+        priority_queue<vector<int>> XPriorityQueue;//Max Priority queue
+        int i=0;
+        for(i=0; i<x; i++)
+        	XPriorityQueue.push(vector<int>{nums[i], numsLen-i});
+        
+        for(i=x; i<k; i++){
+        	XPriorityQueue.push(vector<int>{nums[i], numsLen-i});
+            XPriorityQueue.pop();
+        }
+
+
+        vector<int> temp = XPriorityQueue.top();
+        res.push_back(temp[0]);
+        int QueueSize = x;
+        for(int l=k; l< numsLen; l++)
+        {
+        	//first we push back the lth element into array
+        	XPriorityQueue.push(vector<int>{nums[l], numsLen-l});
+        	QueueSize++;
+        	
+        	vector<int> temp = XPriorityQueue.top();
+        	printf("l: %d\n", l);
+        	printvector(temp);
+        	int indexForThisTemp = numsLen-temp[1];
+        	printf("indexForThisTemp: %d\n", indexForThisTemp);
+        	while(indexForThisTemp<=l-k){
+        		XPriorityQueue.pop();
+        		temp = XPriorityQueue.top();
+        		indexForThisTemp = numsLen-temp[1];
+        	}
+
+        	temp = XPriorityQueue.top();
+
+        	res.push_back(temp[0]);
+        }
+        return(res);
+
+    }
+private:	
+	vector<int> CheckPrime(int x)
+	{
+		vector<int> IsPrime(x+1,0);//0 is excluded, 1 is not prime, 2 is prime
+		IsPrime[2]=1;
+		for(int i=3; i<=x; i++)
+		{
+			int FlagIsPrime=1;
+			for(int j=2; j<=floor(sqrt(i)); j++)
+			{
+				if((IsPrime[j]==1) && (i%j==0))
+					FlagIsPrime=0;
+			}
+			IsPrime[i]=FlagIsPrime;
+		}
+		return(IsPrime);
+	}
+	int IsVowel(char c)
+	{
+		if( c== 'a' || c=='e' ||c=='i' || c=='o' || c=='u')
+			return(1);
+		else
+			return(0);
+	}
+	//ax + by = gcd(a,b)
+	int Simplegcd( int a , int b){
+		if (b==0){
+			return(a);
+		}
+		else{
+			int res = Simplegcd(b, a%b );//the tricky part is that we don’t need to have a>b for the initial input
+			return(res);
+		}
+	}
+	int gcd( int a , int b, int &x , int &y){
+		if (b==0){
+			x=1;
+			y=0;	
+			return(a);
+		}
+		else{
+			int c = a/b;
+			int res = gcd(b, a%b, x , y );//the tricky part is that we don’t need to have a>b for the initial input
+			int tmp = x;
+			x=y;
+			y=tmp-c*y;
+			return(res);
+		}
+	}
+	long long concate(int a, int b){
+		long long res = 0;
+		int digitsOfb = 0;
+		vector<int> test ={1,10,100,1000,10000};
+		for(int i=4; i>=0; i--){
+			if(b/test[i]){
+				digitsOfb = test[i]*10;
+				break;
+			}
+				
+		}
+		res = res + a*digitsOfb + b;
+		return(res);
+	}
+	int score;
+
+    void DFSForMultipleQueryOnGrid(int startx, int starty, int m, int n, vector<vector<int>>& gridVisited, vector<int>& res,
+             vector<int> SortedQueries, int NoQueries, vector<vector<int>>& grid, int ValidQueryIndex,
+               CompareFuncWithParams& CompareFunc) 
+    {
+                // int (*func)(int, int)
+        int k = ValidQueryIndex;
+        printf("ValidQueryIndex: %d: querying: %d\n", ValidQueryIndex, SortedQueries[ValidQueryIndex]);
+        printf("startx: %d; starty: %d\n", startx, starty);
+        fflush(stdout);
+        for(k=ValidQueryIndex;k<NoQueries;k++){
+            if(SortedQueries[k]>grid[startx][starty])
+            {
+                ValidQueryIndex = k;
+                break;
+            }
+        }
+        if(k==NoQueries)
+            return;
+        for(k=ValidQueryIndex; k<NoQueries; k++){
+            res[k]++;
+        }
+        gridVisited[startx][starty]=1;
+        //now we need to find the next smallest box around this box that is not yet visited
+        vector<pair<int,int>> Neighbors;
+        if(startx-1>=0)
+            Neighbors.push_back(pair<int,int>{startx-1, starty});
+        if(startx+1<n)
+            Neighbors.push_back(pair<int,int>{startx+1, starty});
+
+        if(starty-1>=0)
+            Neighbors.push_back(pair<int,int>{startx, starty-1});
+        if(starty+1<m)
+            Neighbors.push_back(pair<int,int>{startx, starty+1});
+
+        sort(Neighbors.begin(), Neighbors.end(), CompareFunc);
+
+        for(auto &Neighbor:Neighbors){
+            int nextx=Neighbor.first;
+            int nexty=Neighbor.second;
+            if(gridVisited[nextx][nexty]==0)
+                DFSForMultipleQueryOnGrid(nextx, nexty, m, n, gridVisited, res, SortedQueries, NoQueries, grid, ValidQueryIndex, CompareFunc);
+        }
+        gridVisited[startx][starty]=2;
+    }
+
+	void DFS(int root, int NoOfCities, vector<vector<pair<int,int>>>& Neighbors, vector<int>& CitiesVisited){
+        CitiesVisited[root]=1;
+        for(const auto &i:Neighbors[root])
+        {
+        	int City=i.first;//the neighbor
+        	int RoadDistance = i.second;//this is the weight assigned to this edge.
+        	if(RoadDistance<score)
+        		score = RoadDistance;
+        	if(CitiesVisited[City]==0)
+        	{
+        		DFS(City, NoOfCities, Neighbors, CitiesVisited);
+        	}	
+        }
+        CitiesVisited[root]=2;
+        return;
+    }
+	
+
+	int IsPrime(char c)
+	{
+		if( c == '2' || c == '3' || c == '5' || c == '7' )
+			return(1);
+		else
+			return(0);
+	}
+	
+	vector<queue<TreeNode*>> TreeBFS(TreeNode* root){
+        queue<TreeNode*> q1;
+        // cout<<"Starting BFS\n"<<endl;
+        // fflush(stdout);
+        vector<queue<TreeNode*>> Res;
+        q1.push(root);
+        do{
+            Res.push_back(q1);
+            queue<TreeNode*> q2;
+            while(!q1.empty()){
+                TreeNode* s = q1.front();
+                q1.pop();
+                if(s->left != nullptr)
+                    q2.push(s->left);
+                if(s->right != nullptr)
+                    q2.push(s->right);
+            }
+            q1 = q2;  
+            // PrintQueueInIntBinaryTree(q1);
+        }while(!q1.empty());
+        return(Res);
+    }
+	//CitiesColor should be initlized to all 2 vector.
+	//MinimumDistance == 501 means that this node is not accessible 
+	//minimumDistance is the distance between root and the current node
+	//notice that this func will not go over whole graph unless the graph is connected.
+	int BFS_Bipartite_Check(int root, int NoOfCities, vector<vector<int>>& Neighbors, vector<int>& MinimumDistanceFromRoot, int& MaximumDistance, vector<int>& CitiesVisited){
+		queue<int> q1;
+		q1.push(root);
+		MinimumDistanceFromRoot[root]=0;//root to root distance is 0;
+		
+		do{
+			queue<int> q2;
+			while(!q1.empty()){
+				int s= q1.front();
+				
+				q1.pop();
+				for(auto &sNext:Neighbors[s]){
+					CitiesVisited[sNext]=CitiesVisited[s];
+					if(MinimumDistanceFromRoot[sNext]==501){//this means that this not has not yet been visited
+						MinimumDistanceFromRoot[sNext] = MinimumDistanceFromRoot[s]+1; 
+						if(MinimumDistanceFromRoot[sNext]>MaximumDistance)
+							MaximumDistance = MinimumDistanceFromRoot[sNext];
+						q2.push(sNext);	
+					}
+					if(MinimumDistanceFromRoot[sNext]%2 == MinimumDistanceFromRoot[s]%2){
+						return(0);//this means that it is not a  bipartite graph
+					}
+				}
+			}
+			q1=q2;	
+		}while(!q1.empty());
+		return(1);
+	}
+};
+
+
+template <size_t rows, size_t cols>
+vector<vector<int>> ArrayTo2dVector(int (&array)[rows][cols]){
+    vector<vector<int>> res;
+    for (size_t i = 0; i < rows; ++i)
+    {
+        vector<int> row;
+        for (size_t j = 0; j < cols; ++j)
+            row.push_back(array[i][j]);
+        res.push_back(row);
+    }
+    return res;
+};
+
+
+
+int main(){
+	Solution mysol;
+	// int inputs[][5]={{0,11,16,5,20},{17,4,19,10,15},{12,1,8,21,6},{3,18,23,14,9},{24,13,2,7,22}};
+	// vector<vector<int>> queries = ArrayTo2dVector(inputs);
+	// int inputs[][3]={{1,2,3},{5,17,7},{9,11,10}};
+	// vector<vector<int>> nums = ArrayTo2dVector(inputs);
+	string word ="abc";
+	// string word ="b";
+	// string word ="aaa";
+	vector<int> nums{1,-1,-3,-2,3};
+	int k= 3;
+	int x=2;
+	// vector<int> nums{-1,-2,-3,-4,-5};
+	// int k= 2;
+	// int x=2;
+	printvector(mysol.getSubarrayBeauty(nums, k, x));
+	
+	
+
+ 	return(0);	
+}
